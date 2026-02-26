beb681d30e5ac5833615839c957ca29cbfcde067
ivan <ivanrsv2000@gmail.com>
2026-02-26 20:30:30 -0300

    Fix errores de configuracion para firebase

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,9 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
-import firebaseConfig from './firebase';
+import { FirebaseApp, initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+import firebaseConfig from "./firebaseConfig"
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const app: FirebaseApp = initializeApp(firebaseConfig)
 
-export default app;
+export const auth = getAuth(app)
+export const db = getFirestore(app)
