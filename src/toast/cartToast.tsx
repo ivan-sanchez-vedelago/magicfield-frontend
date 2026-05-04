f5d415d2cc5774b99648fa54c6c74a2a7151a880
ivan <ivanrsv2000@gmail.com>
2026-05-04 01:13:27 -0300

    Cambios visuales en la burbuja de dialogo de carrito

--- src/toast/cartToast.tsx
+++ src/toast/cartToast.tsx
@@ -10,8 +10,7 @@ export default function CartToast() {
   if (!toastMessage) return null;
 
   return (
-    <div className="cart_overlay">
-      <span className="tip"></span>
+    <div className="cart_overlay dialog_bubble">
       <div className="flex items-center gap-4">
         <ShoppingCart className="w-5 h-5 flex-shrink-0" />
         <div className="flex flex-col">
