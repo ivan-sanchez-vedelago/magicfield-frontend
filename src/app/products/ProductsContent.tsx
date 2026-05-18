ca4d70ed1c319a4904888397937d6b31ab56e933
ivan <ivansa@unitech.com.ar>
2026-05-18 00:47:51 -0300

    Agrego skeletons a la carga inicial de los productos para mejorar la velocidad percibida

--- src/app/products/ProductsContent.tsx
+++ src/app/products/ProductsContent.tsx
@@ -92,7 +92,7 @@ export default function ProductsContent() {
   const { categories } = useCategories();
   const [selectedProduct, setSelectedProduct] = useState<Product | null>(null);
   const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [totalPages, setTotalPages] = useState(0);
   const [totalElements, setTotalElements] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
